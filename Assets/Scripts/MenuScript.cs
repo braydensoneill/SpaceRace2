e7f0221728e7ee9054e1b9574e21c9321ddbd3ff
braydensoneill <braydensoneill@gmail.com>
2022-02-20 23:20:45 +0000

    Added Menu
    Added a fully interactable menu including:
      -Play
      -Settings
      -Scores
      -Exit
    
    Scores/Settings screens are not yet fully interactable. They only contain default values for now

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    //Add more here if you want to add extra scenes
+    enum MenuOptions
+    {
+        Menu = 0,
+        Play = 1,
+    }
+
+    public void Start()
+    {
+        AudioListener.volume = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene((int)MenuOptions.Menu);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene((int)MenuOptions.Play);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
